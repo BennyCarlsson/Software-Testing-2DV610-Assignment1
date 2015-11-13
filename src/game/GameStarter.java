c9f67cc7ead13e2f167ceabf1c811e103400aa03
bc222az@student.lnu.se <bc222az@student.lnu.se>
2015-11-13 16:55:10 +0100

    red

---
+++ src/game/GameStarter.java
@@ -0,0 +1,14 @@
+package game;
+
+public class GameStarter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int testFunctiongetSum(int i, int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
