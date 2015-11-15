ea8064f78b0471fe6f02f09d66036dd5a9589921
bc222az@student.lnu.se <bc222az@student.lnu.se>
2015-11-15 17:22:01 +0100

    RED

--- src/game/GameStarter.java
+++ src/game/GameStarter.java
@@ -6,25 +6,15 @@ import java.util.Scanner;
 public class GameStarter {
 
 	public static void main(String[] args) {
-
-	}
-
-	public int testFunctiongetSum(int i, int j) {
-		int value = i+j;
-		return value;
-	}
-
-	public int test2() {
-		//super advanced calculation
-		int value = 1 + 1;
-		return value;
+		//GameStarter gameStarter = new GameStarter();
+		//gameStarter.chooseGameOption();
 	}
 	
 	public GameOption chooseGameOption() {
 		return chooseGameOption(new Scanner(System.in),new PrintWriter(System.out,true));
 	}
 	public GameOption chooseGameOption(Scanner scanner, PrintWriter output) {
-		output.println("GameOption \n 1. Player vs Computer \n 2. Player vs Player \n 0. Exit");
+		/*output.println("GameOption \n 1. Player vs Computer \n 2. Player vs Player \n 0. Exit");
 		int input;
 		do{
 			input = scanner.nextInt();
@@ -33,8 +23,9 @@ public class GameStarter {
 				case 1: return GameOption.ONEPLAYER;
 				case 2: return GameOption.TWOPLAYER;
 			}
-			output.println("GameOptions \n 1. Player vs Computer \n 2. Player vs Player \n 0. Exit");
+			output.println("No such option try again.. \n GameOptions \n 1. Player vs Computer \n 2. Player vs Player \n 0. Exit");
 		}while(input != 0 || input != 1 || input != 2);
+		return null;*/
 		return null;
 	}
 }
