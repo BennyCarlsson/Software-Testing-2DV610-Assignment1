3307ec707ba1b949bd4f62bc5518585f07781478
bc222az@student.lnu.se <bc222az@student.lnu.se>
2015-11-15 17:35:09 +0100

    RED

--- src/game/GameStarter.java
+++ src/game/GameStarter.java
@@ -6,15 +6,16 @@ import java.util.Scanner;
 public class GameStarter {
 
 	public static void main(String[] args) {
-		//GameStarter gameStarter = new GameStarter();
-		//gameStarter.chooseGameOption();
+		GameStarter gameStarter = new GameStarter();
+		gameStarter.chooseGameOption();
 	}
 	
 	public GameOption chooseGameOption() {
 		return chooseGameOption(new Scanner(System.in),new PrintWriter(System.out,true));
 	}
 	public GameOption chooseGameOption(Scanner scanner, PrintWriter output) {
-		/*output.println("GameOption \n 1. Player vs Computer \n 2. Player vs Player \n 0. Exit");
+		/*
+		output.println("GameOption \n 1. Player vs Computer \n 2. Player vs Player \n 0. Exit");
 		int input;
 		do{
 			input = scanner.nextInt();
@@ -25,7 +26,8 @@ public class GameStarter {
 			}
 			output.println("No such option try again.. \n GameOptions \n 1. Player vs Computer \n 2. Player vs Player \n 0. Exit");
 		}while(input != 0 || input != 1 || input != 2);
-		return null;*/
+		return null;
+		*/
 		return null;
 	}
 }
--- tests/game/GameStarterTest.java
+++ tests/game/GameStarterTest.java
@@ -24,9 +24,10 @@ public class GameStarterTest {
 	public void testChooseIllegalGameOption(){
 		GameStarter gameStarter = new GameStarter();
 		StringWriter output = new StringWriter();
-		String input = "4";
+		String input = "4\n" + "1\n";
 		GameOption gameOption = gameStarter.chooseGameOption(new Scanner(input),new PrintWriter(output));
-		assertTrue(output.toString().contains("No such option try again.."));
+		assertTrue(output.toString().contains("No such option try again.. \n GameOptions \n 1. Player vs Computer \n 2. Player vs Player \n 0. Exit"));
+		assertEquals(GameOption.ONEPLAYER, gameOption);
 	}
 
 }
