0983a0a11cfb6ab3cd04d28acea7a2580bf3d70b
bc222az@student.lnu.se <bc222az@student.lnu.se>
2015-11-16 11:02:45 +0100

    GREEN

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -3,6 +3,9 @@ package game;
 public class GameBoard {
 	BoardState[] BoardArray;
 	public GameBoard(){
-		
+		BoardArray = new BoardState[]{BoardState.EMPTY,BoardState.EMPTY,
+									BoardState.EMPTY,BoardState.EMPTY,BoardState.EMPTY,
+									BoardState.EMPTY,BoardState.EMPTY,BoardState.EMPTY,
+									BoardState.EMPTY};
 	}
 }
