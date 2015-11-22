c5cf4a60816d0beec013036b3132ddf932bbeb39
bc222az@student.lnu.se <bc222az@student.lnu.se>
2015-11-22 17:47:29 +0100

    RED

--- src/game/GameRules.java
+++ src/game/GameRules.java
@@ -12,21 +12,12 @@ public class GameRules {
 		return false;
 	}
 	public boolean checkWinner(BoardState[] boardArray){
-		if(isBoardStateSame(boardArray[0], boardArray[1], boardArray[2])
-		|| isBoardStateSame(boardArray[3], boardArray[4], boardArray[5])
-		|| isBoardStateSame(boardArray[6], boardArray[7], boardArray[8])
-		|| isBoardStateSame(boardArray[0], boardArray[3], boardArray[6])
-		|| isBoardStateSame(boardArray[1], boardArray[4], boardArray[5])
-		|| isBoardStateSame(boardArray[2], boardArray[5], boardArray[8])
-		|| isBoardStateSame(boardArray[0], boardArray[4], boardArray[8])
-		|| isBoardStateSame(boardArray[2], boardArray[4], boardArray[6])){
-			return true;
-		}
+		
 		return false;
 	}
 	public boolean isBoardStateSame(BoardState bs1,BoardState bs2, BoardState bs3){
 		
-		return true;
+		return false;
 	}
 	public boolean checkTie(BoardState[] boardArray){
 		return false;
--- tests/game/GameRulesTests.java
+++ tests/game/GameRulesTests.java
@@ -40,12 +40,26 @@ public class GameRulesTests {
 		verify(gameRules,atLeastOnce()).checkWinner(boardArray);
 	}
 	@Ignore@Test
-	public void testCheckWinnerNoWinner(){
-		
-	}
-	@Test
 	public void testCheckWinnerWithWinner(){
 		assertTrue(_gameRules.checkWinner(boardArrayWinner));
+	}
+	@Test
+	public void testCheckWinnerUsesisBoardStateSame(){
+		BoardState[] specialBoardArray = new BoardState[]{
+				BoardState.X,BoardState.EMPTY,BoardState.O,
+				BoardState.EMPTY,BoardState.O,BoardState.X,
+				BoardState.O,BoardState.X,BoardState.EMPTY};
+		
+		GameRules gameRules = spy(new GameRules());
+		gameRules.checkWinner(specialBoardArray);
+		verify(gameRules,atLeastOnce()).isBoardStateSame(BoardState.X,BoardState.EMPTY,BoardState.O);
+		verify(gameRules,atLeastOnce()).isBoardStateSame(BoardState.EMPTY,BoardState.O,BoardState.X);
+		verify(gameRules,atLeastOnce()).isBoardStateSame(BoardState.O,BoardState.X,BoardState.EMPTY);
+		verify(gameRules,atLeastOnce()).isBoardStateSame(BoardState.X,BoardState.EMPTY,BoardState.O);
+		verify(gameRules,atLeastOnce()).isBoardStateSame(BoardState.EMPTY,BoardState.O,BoardState.X);
+		verify(gameRules,atLeastOnce()).isBoardStateSame(BoardState.O,BoardState.X,BoardState.EMPTY);
+		verify(gameRules,atLeastOnce()).isBoardStateSame(BoardState.X,BoardState.O,BoardState.EMPTY);
+		verify(gameRules,atLeastOnce()).isBoardStateSame(BoardState.O,BoardState.O,BoardState.O);
 	}
 	@Ignore@Test
 	public void testTieWithTie(){
