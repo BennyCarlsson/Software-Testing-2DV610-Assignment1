a576ed0bb7c440ec967554942dba08dc44cfe2ff
bc222az@student.lnu.se <bc222az@student.lnu.se>
2015-11-27 13:29:52 +0100

    RED

--- src/game/GameStarter.java
+++ src/game/GameStarter.java
@@ -4,26 +4,39 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class GameStarter {
-
+	GameBoard gameBoard;
+	Player player1;
+	Player player2;
+	GameRules gameRules;
+	Player playerStarted;
+	Player playerTurn;
 	public static void main(String[] args) {
 		GameStarter gameStarter = new GameStarter();
-		gameStarter.runGame();
+		gameStarter.startGame();
+	}
+	public void startGame(){
+		//Instansiate GameBoard
+		//Instansiate Player1
+		//Instansiate Player2
+		//Instansiate GameRules
+		//runGame();
 	}
 	public void runGame(){
 		//Call chooseGameOption
 		//Set PlayMode or exit
-		//Instansiate GameBoard
-		//Instansiate Player
-		//Instansiate GamRules
-		
 		//while loop
-		//DisplayBoard
-		//Ask Player 1/2 input
-		// if legalInput
-			//convertInputToBoardPosition
-		// ifcanPlay
-			//editBoard
-		//if Winner/Tie
-			//play again?
+			//DisplayBoard
+			//playTurn()
+			//if Winner/Tie
+				//play again?
+	}
+	public void playTurn(){
+		//while
+			//Ask Player 1/2 input
+			// if legalInput
+				//convertInputToBoardPosition
+			// ifcanPlay
+				//editBoard
+				//return
 	}
 }
--- tests/game/GameStarterTests.java
+++ tests/game/GameStarterTests.java
@@ -8,9 +8,19 @@ import java.util.Scanner;
 
 import org.junit.Ignore;
 import org.junit.Test;
-
+import static org.mockito.Mockito.*;
 public class GameStarterTests {
 
-	
+	@Test
+	public void startGame(){
+		GameStarter gameStarter = new GameStarter();
+		gameStarter.startGame();
+		assertNotNull(gameStarter.gameBoard);
+		assertNotNull(gameStarter.player1);
+		assertNotNull(gameStarter.player2);
+		assertNotNull(gameStarter.gameRules);
+		assertSame(gameStarter.player1, gameStarter.playerStarted);
+		assertSame(gameStarter.player1, gameStarter.playerTurn);
+	}
 
 }
