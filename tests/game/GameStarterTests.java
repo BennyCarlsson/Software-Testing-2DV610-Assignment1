5d268b0098927894b93026795a388463df81715e
bc222az@student.lnu.se <bc222az@student.lnu.se>
2015-11-27 17:12:31 +0100

    RED

--- tests/game/GameStarterTests.java
+++ tests/game/GameStarterTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 import org.junit.Before;
@@ -11,6 +12,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.Any;
 
 import static org.mockito.Mockito.*;
 public class GameStarterTests {
@@ -40,5 +42,20 @@ public class GameStarterTests {
 		verify(mockGameRules,times(1)).chooseGameOption();
 		assertEquals(GameOption.TWOPLAYER, gameStarter.gameOption);
 	}
-
+	
+	@Test
+	public void runGameTest(){
+		GameStarter gameStarter = new GameStarter(){
+			public void playTurn(){
+				
+			}
+		};
+		gameStarter.gameBoard = new GameBoard();
+		gameStarter.setGameRules(mockGameRules);
+		gameStarter.runGame();
+		when(mockGameRules.checkWinner(gameStarter.gameBoard.boardArray)).thenReturn(false).thenReturn(false);
+		when(mockGameRules.checkTie(gameStarter.gameBoard.boardArray)).thenReturn(false).thenReturn(true);
+		verify(mockGameRules,times(2)).checkWinner(gameStarter.gameBoard.boardArray);
+		verify(mockGameRules,times(2)).checkWinner(gameStarter.gameBoard.boardArray);
+	}
 }
